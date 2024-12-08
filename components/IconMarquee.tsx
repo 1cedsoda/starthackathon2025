bbc51caad8cd662d0517cb3b517273ef0f48d327
Icedsoda <29167146+1cedsoda@users.noreply.github.com>
2024-12-08 11:58:39 +0100

    more icons

--- components/IconMarquee.tsx
+++ components/IconMarquee.tsx
@@ -9,17 +9,29 @@ import Slack from "@/public/slack.svg";
 import Whatsapp from "@/public/whatsapp.svg";
 import Confluence from "@/public/confluence.svg";
 import Jira from "@/public/jira-1.svg";
+import Onedrive from "@/public/onedrive.svg";
+import Gmail from "@/public/gmail.svg";
+import Outlok from "@/public/outlook.svg";
+import Teams from "@/public/teams.svg";
+import GoogleDrive from "@/public/google-drive.svg";
+import Notion from "@/public/notion.svg";
 import { useMemo } from "react";
 
 const icons = [
-  { id: "discord", component: <Discord width={48} height={48} /> },
-  { id: "hubspot", component: <HubSpot width={48} height={48} /> },
-  { id: "salesforce", component: <Salesforce width={48} height={48} /> },
-  { id: "sap", component: <SAP width={48} height={48} /> },
-  { id: "slack", component: <Slack width={48} height={48} /> },
-  { id: "whatsapp", component: <Whatsapp width={48} height={48} /> },
-  { id: "confluence", component: <Confluence width={48} height={48} /> },
-  { id: "jira", component: <Jira width={48} height={48} /> },
+  { id: "discord", component: <Discord width={64} height={64} /> },
+  { id: "hubspot", component: <HubSpot width={64} height={64} /> },
+  { id: "salesforce", component: <Salesforce width={64} height={64} /> },
+  { id: "sap", component: <SAP width={64} height={64} /> },
+  { id: "slack", component: <Slack width={64} height={64} /> },
+  { id: "whatsapp", component: <Whatsapp width={64} height={64} /> },
+  { id: "confluence", component: <Confluence width={64} height={64} /> },
+  { id: "jira", component: <Jira width={64} height={64} /> },
+  { id: "onedrive", component: <Onedrive width={64} height={64} /> },
+  { id: "gmail", component: <Gmail width={64} height={64} /> },
+  { id: "outlook", component: <Outlok width={64} height={64} /> },
+  { id: "teams", component: <Teams width={64} height={64} /> },
+  { id: "google-drive", component: <GoogleDrive width={64} height={64} /> },
+  { id: "notion", component: <Notion width={64} height={64} /> },
 ];
 
 const IconCard = ({ icon }: { icon: React.ReactNode }) => {
